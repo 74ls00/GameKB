05a6a11926cdcdac812005f814ae37bb6ea20754
74ls00 <userled@yandex.ru>
2016-08-06 01:34:14 +0300

    del 1 wire

--- firmware/EasyAVR/keymapper/easykeymap/boards/korbord_num - копия (2).py
+++ firmware/EasyAVR/keymapper/easykeymap/boards/korbord_num - копия (2).py
@@ -16,13 +16,6 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# убрана 1 линия для добавления светодиода:
-# num_cols заменено 18 на 17 , убрано REF_PORTB 7 #( REF_PORTB , (1 << 7) ),
-# добавлено num в led_definition и led_hardware
-# изменены линии в keyboard_definition 15 на 14, 16 на 15, 17 на 16 (8 изменений в каждом)
-
-
 
 """Keyboard definition for Filco HID Lib keyboard"""
 
@@ -40,7 +33,7 @@ display_height = int(6.5*4)
 display_width = int(20.25*4)
 
 num_rows = 8
-num_cols = 17
+num_cols = 18
 
 strobe_cols = False
 strobe_low = False
@@ -69,6 +62,7 @@ matrix_strobe = [
 matrix_sense = [
 #      Port        Pin mask
     ( REF_PORTB , (1 << 4) ),
+    ( REF_PORTB , (1 << 7) ), # скорее всего нужно указать num_cols меньше и  уменьшать номера в таблице
     ( REF_PORTC , (1 << 6) ),
     ( REF_PORTC , (1 << 7) ),
     ( REF_PORTD , (1 << 0) ),
@@ -116,9 +110,9 @@ KMAC_key = None
 
 # (горизонталь кнопки, вертикаль кнопки), (?выходы дешифратора, входы контролера)  
 # цепочка входов по схеме нумпада
-# A0 = 20 = PD2 - 3,4,5   != 3D2_6  = 16
-# A1 = 21 = PD3 - 9,10,11 != 3D2_8  = 15
-# A2 = 19 = PD1 - 1,2,13  != 3D2_12 = 14
+# A0 = 20 = PD2 - 3,4,5   != 3D2_6  = 17
+# A1 = 21 = PD3 - 9,10,11 != 3D2_8  = 16
+# A2 = 19 = PD1 - 1,2,13  != 3D2_12 = 15
 #
 # номер порта дешифратора | выход дешифратора | номер порта в коде
 # Y0 = 15 = 0
@@ -130,53 +124,55 @@ KMAC_key = None
 # Y6 = 9  = 3
 # Y7 = 7  = 7
 
+
+
 keyboard_definition = [
 	[
 
-((4, 4), (5, 14), 'HID_KEYBOARD_SC_NUM_LOCK'),                 #OK
-((4, 4), (7, 14), 'HID_KEYBOARD_SC_D'),	                       #OK
-((4, 4), (5, 16), 'HID_KEYBOARD_SC_E'),                        #OK
-((4, 4), (5, 15), 'HID_KEYBOARD_SC_F'),                        #OK
-((4, 4), (1, 15), 'HID_KEYBOARD_SC_KEYPAD_SLASH')              #OK
+((4, 4), (5, 15), 'HID_KEYBOARD_SC_NUM_LOCK'),                 #OK
+((4, 4), (7, 15), 'HID_KEYBOARD_SC_D'),	                       #OK
+((4, 4), (5, 17), 'HID_KEYBOARD_SC_E'),                        #OK
+((4, 4), (5, 16), 'HID_KEYBOARD_SC_F'),                        #OK
+((4, 4), (1, 16), 'HID_KEYBOARD_SC_KEYPAD_SLASH')              #OK
 	 ],
 	 #2,
 	 [
    
 (4, None, '0'),		 
-((4, 4), (3, 14), 'HID_KEYBOARD_SC_A'),                        #OK
-((4, 4), (7, 16), 'HID_KEYBOARD_SC_B'),	                       #OK
-((4, 4), (7, 15), 'HID_KEYBOARD_SC_C'),                        #OK
-((4, 4), (6, 15), 'HID_KEYBOARD_SC_KEYPAD_ASTERISK')	       #OK
+((4, 4), (3, 15), 'HID_KEYBOARD_SC_A'),                        #OK
+((4, 4), (7, 17), 'HID_KEYBOARD_SC_B'),	                       #OK
+((4, 4), (7, 16), 'HID_KEYBOARD_SC_C'),                        #OK
+((4, 4), (6, 16), 'HID_KEYBOARD_SC_KEYPAD_ASTERISK')	       #OK
      ],[
 
 (4, None, '0'),		 
-((4, 4), (1, 14), 'HID_KEYBOARD_SC_KEYPAD_7_AND_HOME'),        #OK
-((4, 4), (3, 16), 'HID_KEYBOARD_SC_KEYPAD_8_AND_UP_ARROW'),    #OK
-((4, 4), (3, 15), 'HID_KEYBOARD_SC_KEYPAD_9_AND_PAGE_UP'),	   #OK
-((4, 4), (2, 15), 'HID_KEYBOARD_SC_KEYPAD_MINUS')	           #OK
+((4, 4), (1, 15), 'HID_KEYBOARD_SC_KEYPAD_7_AND_HOME'),        #OK
+((4, 4), (3, 17), 'HID_KEYBOARD_SC_KEYPAD_8_AND_UP_ARROW'),    #OK
+((4, 4), (3, 16), 'HID_KEYBOARD_SC_KEYPAD_9_AND_PAGE_UP'),	   #OK
+((4, 4), (2, 16), 'HID_KEYBOARD_SC_KEYPAD_MINUS')	           #OK
      ],[
 
 (4, None, '0'),	 	 
-((4, 4), (6, 14), 'HID_KEYBOARD_SC_KEYPAD_4_AND_LEFT_ARROW'),  #OK
-((4, 4), (1, 16), 'HID_KEYBOARD_SC_KEYPAD_5'),                 #OK
-((4, 4), (2, 16), 'HID_KEYBOARD_SC_KEYPAD_6_AND_RIGHT_ARROW'), #OK
-((4, 4), (4, 15), 'HID_KEYBOARD_SC_KEYPAD_PLUS')	 #4,4,7,2  #OK
+((4, 4), (6, 15), 'HID_KEYBOARD_SC_KEYPAD_4_AND_LEFT_ARROW'),  #OK
+((4, 4), (1, 17), 'HID_KEYBOARD_SC_KEYPAD_5'),                 #OK
+((4, 4), (2, 17), 'HID_KEYBOARD_SC_KEYPAD_6_AND_RIGHT_ARROW'), #OK
+((4, 4), (4, 16), 'HID_KEYBOARD_SC_KEYPAD_PLUS')	 #4,4,7,2  #OK
      ],[
 (4, None, '0'),
 
-((4, 4), (2, 14), 'HID_KEYBOARD_SC_KEYPAD_1_AND_END'),         #OK
-((4, 4), (6, 16), 'HID_KEYBOARD_SC_KEYPAD_2_AND_DOWN_ARROW'),  #OK
-((4, 4), (4, 16), 'HID_KEYBOARD_SC_KEYPAD_3_AND_PAGE_DOWN'),   #OK
-((4, 8), (0, 15), 'HID_KEYBOARD_SC_KEYPAD_ENTER')              #OK
+((4, 4), (2, 15), 'HID_KEYBOARD_SC_KEYPAD_1_AND_END'),         #OK
+((4, 4), (6, 17), 'HID_KEYBOARD_SC_KEYPAD_2_AND_DOWN_ARROW'),  #OK
+((4, 4), (4, 17), 'HID_KEYBOARD_SC_KEYPAD_3_AND_PAGE_DOWN'),   #OK
+((4, 8), (0, 16), 'HID_KEYBOARD_SC_KEYPAD_ENTER')              #OK
 #((4, 4), (4, 3), 'HID_KEYBOARD_SC_NON_US_HASHMARK_AND_TILDE'),
 #(13, None, '0'),
 #((4, 4), (1, 7), 'HID_KEYBOARD_SC_NON_US_BACKSLASH_AND_PIPE'),
      ],[
 
 (4, None, '0'),
-((4, 4), (4, 14), 'HID_KEYBOARD_SC_KEYPAD_0_AND_INSERT'),      #OK
-((4, 4), (0, 14), 'HID_KEYBOARD_SC_KEYPAD_DOT_AND_DELETE'),    #OK
-((4, 4), (0, 16), 'HID_KEYBOARD_SC_SLASH_AND_QUESTION_MARK')   #OK
+((4, 4), (4, 15), 'HID_KEYBOARD_SC_KEYPAD_0_AND_INSERT'),      #OK
+((4, 4), (0, 15), 'HID_KEYBOARD_SC_KEYPAD_DOT_AND_DELETE'),    #OK
+((4, 4), (0, 17), 'HID_KEYBOARD_SC_SLASH_AND_QUESTION_MARK')   #OK
      ]
 ]
 
