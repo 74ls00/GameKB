73609dba2cb10bcc1c07dbb8ec9ae734ef27d23d
74ls00 <userled@yandex.ru>
2016-08-22 19:55:00 +0300

    rus fix

--- firmware/avrdude_EasyAVR_32u4write - NumPad.bat
+++ firmware/avrdude_EasyAVR_32u4write - NumPad.bat
@@ -2,7 +2,7 @@ set avrdude=avrdude
 set dir=leonardo
 set port=com20
 rem flash=
-set fw=G:\home\Documents\Projects\EAGLE\Keyboard\firmware\EasyAVR\EasyAVR_NumPad_def_fw_c.hex
+set fw=G:\home\Documents\Projects\EAGLE\Keyboard\firmware\EasyAVR\EasyAVR_Num+B_def_fw_c.hex
 
 rem avrdude.exe -P %port% -c stk500v2 -p m32u4 -v F
 %avrdude%\avrdude.exe -P %port% -c stk500v2 -p m32u4 -U flash:w:%fw%:a
