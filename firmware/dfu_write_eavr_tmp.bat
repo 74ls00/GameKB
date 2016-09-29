9b1d5b96d7b3da663b2a7ee335cb238e97295f80
74ls00 <userled@yandex.ru>
2016-09-30 01:56:26 +0300

    .

--- firmware/dfu_write_eavr_tmp.bat
+++
@@ -1,9 +0,0 @@
-set dfudir=dfu-programmer-win-0.7.2
-set controller=atmega32u4
-set fw=EasyAVR\keymapper\tmp.hex
-
-%dfudir%\dfu-programmer.exe %controller% erase --force
-%dfudir%\dfu-programmer.exe %controller% flash %fw%
-%dfudir%\dfu-programmer.exe %controller% reset
-
-pause
--- firmware/run-port.bat
+++
@@ -1,5 +0,0 @@
-path=G:\home\Documents\Projects\EAGLE\Keyboard\firmware\PythonPortable\App;%path%
-cd /d G:\home\Documents\Projects\EAGLE\Keyboard\firmware\EasyAVR\keymapper
-python.exe -m easykeymap.gui
-
-@pause
